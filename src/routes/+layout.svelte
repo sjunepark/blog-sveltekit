3a2c267cf2e0b0cd461883c3789e8269354f3e6c
June Park <junepark202012@gmail.com>
2024-03-02 16:36:00 +0900

    style: adjust styles for icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
 	import ThemeSwitcher from './navbar/ThemeSwitcher.svelte';
+	// noinspection ES6UnusedImports
+	import * as Popover from '$lib/shadcn/ui/popover';
+	import { Button } from '$lib/shadcn/ui/button';
+	import HamburgerMenu from 'svelte-radix/HamburgerMenu.svelte';
 
 	let { children } = $props();
 </script>
@@ -13,17 +17,31 @@
 		<div class="w-full bg-background ring-1 ring-border"></div>
 	</div>
 	<header
-		class="sticky top-0 z-50 mx-auto flex w-full max-w-screen-lg items-center justify-end gap-4 bg-gradient-to-b from-background from-65% via-background/30 via-90% px-4 pb-12 pt-6 sm:px-8 md:justify-center md:px-12 md:pb-16"
+		class="sticky top-0 z-50 mx-auto flex w-full max-w-screen-lg items-center justify-end gap-4 bg-gradient-to-b from-background from-65% via-background/30 via-90% px-4 pb-12 pt-6 sm:justify-center sm:px-8 md:px-12 md:pb-16"
 	>
 		<div class="flex flex-1">Home</div>
 		<!--MobileNavigation-->
-		<nav class="hidden md:block">
+		<nav class="sm:hidden">
+			<Popover.Root>
+				<Popover.Trigger>
+					<Button
+						variant="outline"
+						size="icon"
+						class="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+						><HamburgerMenu /></Button
+					>
+				</Popover.Trigger>
+				<Popover.Content>Place content for the popover here.</Popover.Content>
+			</Popover.Root>
+		</nav>
+		<!--DesktopNavigation-->
+		<nav class="hidden sm:block">
 			<ul class="flex rounded-full px-3 font-medium shadow-md shadow-border ring-1 ring-border">
 				<li><a href="/" class="block px-3 py-2">About</a></li>
 				<li><a href="/" class="block px-3 py-2">Projects</a></li>
 			</ul>
 		</nav>
-		<div class="flex justify-end md:flex-1">
+		<div class="flex flex-1 justify-end">
 			<ThemeSwitcher />
 		</div>
 	</header>
--- src/routes/navbar/ThemeSwitcher.svelte
+++ src/routes/navbar/ThemeSwitcher.svelte
@@ -12,16 +12,16 @@
 	<DropdownMenu.Trigger asChild let:builder>
 		<Button
 			aria-label="Switch light/dark theme"
-			class="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+			class="group rounded-full bg-secondary/80 px-3 py-2 shadow-lg shadow-foreground/5 ring-1 ring-border/5 backdrop-blur transition hover:bg-secondary/20 hover:ring-foreground/50"
 			builders={[builder]}
 			variant="outline"
-			size="icon"
+			size="icon_lg"
 		>
 			<Sun
-				class="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+				class="absolute h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
 			/>
 			<Moon
-				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+				class="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
 			/>
 			<span class="sr-only">Toggle theme</span>
 		</Button>
