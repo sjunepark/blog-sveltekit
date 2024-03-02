85edae73190636b0a6929983b7bc2ee6786db24a
June Park <junepark202012@gmail.com>
2024-03-02 09:19:05 +0900

    feat: use tailwindcss and shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import { Input } from '$lib/shadcn/components/ui/input';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Input />
