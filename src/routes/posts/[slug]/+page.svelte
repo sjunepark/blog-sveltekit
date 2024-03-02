7742c601046838ffb6629b12db2bd20a0bdd179d
June Park <junepark202012@gmail.com>
2024-03-02 17:23:56 +0900

    feat: implement mdsvex

---
+++ src/routes/posts/[slug]/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<!-- SEO -->
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={data.meta.title} />
+</svelte:head>
+
+<article>
+	<!-- Title -->
+	<hgroup>
+		<h1>{data.meta.title}</h1>
+		<p>Published at {data.meta.date}</p>
+	</hgroup>
+
+	<!-- Tags -->
+	<div class="">
+		{#each data.meta.categories as category}
+			<span class="">&num;{category}</span>
+		{/each}
+	</div>
+
+	<!-- Post -->
+	<div class="prose prose-stone dark:prose-invert">
+		<svelte:component this={data.content} />
+	</div>
+</article>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,31 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex, escapeSvelte } from 'mdsvex';
+import { getHighlighter } from 'shiki';
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await getHighlighter({
+				themes: ['monokai'],
+				langs: ['javascript', 'typescript', 'go']
+			});
+
+			await highlighter.loadLanguage('javascript', 'typescript', 'go');
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'monokai' }));
+			return `{@html \`${html}\`}`;
+		}
+	}
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
