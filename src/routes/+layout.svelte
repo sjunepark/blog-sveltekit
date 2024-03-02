df8783c124959456967cc2c4a760ad8e2c1b370e
June Park <junepark202012@gmail.com>
2024-03-03 06:01:31 +0900

    style: increase width of header element

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,44 +11,47 @@
 </script>
 
 <ModeWatcher />
-<div class="relative mx-auto grid w-full max-w-7xl px-4 sm:px-8 md:px-12">
+<div class="relative mx-auto min-h-full w-full max-w-7xl flex-col px-4 sm:px-8 md:px-12">
 	<!-- Background -->
 	<div class="absolute inset-0 -z-50 mx-auto flex w-full justify-center px-4 sm:px-8 md:px-12">
 		<div class="w-full bg-background ring-1 ring-border"></div>
 	</div>
 	<header
-		class="sticky top-0 z-50 mx-auto flex w-full max-w-screen-lg items-center justify-end gap-4 bg-gradient-to-b from-background from-65% via-background/30 via-90% px-4 pb-12 pt-6 sm:justify-center sm:px-8 md:px-12 md:pb-16"
+		class="sticky top-0 z-50 w-full bg-gradient-to-b from-background from-65% via-background/30 via-90% px-4 pb-12 pt-6 sm:px-8 md:px-12 md:pb-16"
 	>
-		<div class="flex flex-1">Home</div>
-		<!--MobileNavigation-->
-		<nav class="sm:hidden">
-			<Popover.Root>
-				<Popover.Trigger>
-					<Button
-						variant="outline"
-						size="icon"
-						class="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-foreground/5 ring-1 ring-border/5 backdrop-blur transition"
-					>
-						<HamburgerMenu />
-					</Button>
-				</Popover.Trigger>
-				<Popover.Content>Place content for the popover here.</Popover.Content>
-			</Popover.Root>
-		</nav>
-		<!--DesktopNavigation-->
-		<nav class="hidden sm:block">
-			<ul class="flex rounded-full px-3 font-medium shadow-md shadow-border ring-1 ring-border">
-				<li>
-					<a href="/" class="block px-3 py-2 transition duration-200 hover:text-primary">About</a>
-				</li>
-				<li>
-					<a href="/" class="block px-3 py-2 transition duration-200 hover:text-primary">Projects</a
-					>
-				</li>
-			</ul>
-		</nav>
-		<div class="flex flex-1 justify-end">
-			<ThemeSwitcher />
+		<div class="mx-auto flex max-w-screen-lg items-center justify-end gap-4 sm:justify-center">
+			<div class="flex flex-1">Home</div>
+			<!--MobileNavigation-->
+			<nav class="sm:hidden">
+				<Popover.Root>
+					<Popover.Trigger>
+						<Button
+							variant="outline"
+							size="icon"
+							class="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-foreground/5 ring-1 ring-border/5 backdrop-blur transition"
+						>
+							<HamburgerMenu />
+						</Button>
+					</Popover.Trigger>
+					<Popover.Content>Place content for the popover here.</Popover.Content>
+				</Popover.Root>
+			</nav>
+			<!--DesktopNavigation-->
+			<nav class="hidden sm:block">
+				<ul class="flex rounded-full px-3 font-medium shadow-md shadow-border ring-1 ring-border">
+					<li>
+						<a href="/" class="block px-3 py-2 transition duration-200 hover:text-primary">About</a>
+					</li>
+					<li>
+						<a href="/" class="block px-3 py-2 transition duration-200 hover:text-primary"
+							>Projects</a
+						>
+					</li>
+				</ul>
+			</nav>
+			<div class="flex flex-1 justify-end">
+				<ThemeSwitcher />
+			</div>
 		</div>
 	</header>
 	<main class="mx-auto w-full px-4 sm:px-8 md:px-12">
